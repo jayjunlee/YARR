b3b9149279aa28138820247c002f636bf7676a3d
Jayjun Lee <jl7719@ic.ac.uk>
2024-03-07 22:14:04 -0500

    add more perturbation than simple x and y

--- yarr/utils/rollout_generator.py
+++ yarr/utils/rollout_generator.py
@@ -23,6 +23,8 @@ from yarr.agents.agent import ActResult
 from rvt.utils.peract_utils import CAMERAS
 from rvt.libs.peract.helpers.demo_loading_utils import keypoint_discovery
 from waypoint_extraction.extract_waypoints import dp_waypoint_selection
+from rvt.mvt.aug_utils import quaternion_to_discrete_euler, sensitive_gimble_fix, discrete_euler_to_quaternion, quaternion_to_euler
+from scipy.spatial.transform import Rotation
 
 import os
 import shutil
@@ -51,6 +53,7 @@ class RolloutGenerator(object):
                   eval: bool, log_dir, task_name, episode_number, eval_demo_seed: int = 0,
                   record_enabled: bool = False,
                   replay_ground_truth: bool = False,
+                  perturb: bool = False,
                   interactive: bool = False
                   ):
         # 1. reset
@@ -73,32 +76,96 @@ class RolloutGenerator(object):
         user_has_control = False
 
         # perturbation injection
-        # waypoints: range(0,whatever)
-        # dense_actions: shape (186,9)
-        # keypoints: [56, 79, 101, 138, 147, 185]
+        # keypoints: [56, 79, 101, 138, 147, 185] (x start from 0)
         # the perturbed idx is where failure is injected. we fallback to a previous waypoint and reapproach the same waypoint correctly
-        perturbed_idx = list(range(len(keypoints))) # perurb 79 = keypoints[peturbed_idx[0]]
-        p = []
-        p_count = 0 
-        for idx in perturbed_idx:
-            # inject perturbation to expert keypoint to induce failure
-            perturbed_action = actions[p_count * 2 + idx,:].copy()
-            perturbation = np.random.normal(0, 0.05, size=(2,))
-            p.append(perturbation)
-            perturbed_action[0:2] += perturbation
-            perturbed_action[2] += 0.02
+        # Currently checks the following:
+        # 1. Perturb rotation for all roll, pitch, yaw.
+        #   -> Or do we want to focus on the yaw perturbations? The yaw alignment issue was prevalent in picking up action failure modes.
+        # 2. Toggle gripper by 0.1 probability.
+        # 3. Perturb i) x, y only, ii) z only, iii) x, y, z by small amount. Chosen uniform random.
+        #   -> One problem in iii) is that when the correction happens it may cause the model to learn go backwards on step?
+        # 4. If the last keypoint is rotation-only transition, don't perturb. Likely to be "screw" action.
+        perturbed_idx = []
+        corrective_idx = []
+        resolution = 5
 
-            # randomly sample a correct waypoint from a segment of waypoints leading up to the expert keypoint
-            if idx == 0:
-                waypoint_segment = waypoints[0 : keypoints[idx]]
-            else:
-                waypoint_segment = waypoints[keypoints[idx-1] : keypoints[idx]]
-            corrective_action = dense_actions[np.random.choice(waypoint_segment)]
+        # noise params
+        translation_noise = 0.1
+        rotation_noise = 45
 
-            # insert perturbed action and correction action to actions
-            actions = np.insert(actions, p_count * 2 + idx, np.vstack([perturbed_action, corrective_action]), axis=0)
-            p_count += 1
-        print(np.round(actions,3))
+        if replay_ground_truth & perturb:
+            perturbed_idx = list(range(len(keypoints))) # perurb 79 = keypoints[peturbed_idx[0]]
+            p_count = 0 
+            for i, idx in enumerate(perturbed_idx):
+                # Perturbation injection to expert keypoint to induce failure
+                perturbed_action = actions[idx + p_count * 2,:].copy()
+
+                # 4.
+                if i == len(perturbed_idx) - 1:
+                    prev_action = actions[perturbed_idx[idx-1] + p_count * 2,:].copy()
+                    print(np.sum(np.abs(prev_action[0:2] - perturbed_action[0:2])))
+                    if np.sum(np.abs(prev_action[0:2] - perturbed_action[0:2])) < 0.001:
+                        print("continue")
+                        perturbed_idx.remove(idx)
+                        break
+
+                # 1. 
+                euler = quaternion_to_euler(perturbed_action[3:7], resolution).astype(np.float64)
+                rot_perturbation = np.random.normal(0, rotation_noise, size=(1,))
+                euler[2] += rot_perturbation # only yaw
+                disc = Rotation.from_euler("xyz", euler, degrees=True).as_quat()
+                perturbed_action[3:7] = disc
+
+                # 2.
+                gripper_toggle = np.random.choice([0, 1], p=[0.9, 0.1])
+                if gripper_toggle:
+                    if perturbed_action[8]:
+                        perturbed_action[8] = 0.0
+                    else:
+                        perturbed_action[8] = 1.0
+                
+                # TODO Write function that checks for IK bound given x y z.
+
+                # 3.
+                # translation_perturbation_mode = np.random.choice([0, 1, 2])
+                # # for debugging; comment out when running data aug
+                # translation_perturbation_mode = 2
+                # if translation_perturbation_mode == 0:
+                #     # perturb only x and y
+                #     xy_perturbation = np.random.normal(0, 0.05, size=(2,))
+                #     perturbed_action[0:2] += xy_perturbation
+                # elif translation_perturbation_mode == 1:
+                #     # perturb only z
+                #     z_perturbation = -1
+                #     while z_perturbation <= 0:
+                #         z_perturbation = np.random.normal(0, 0.1)
+                #     perturbed_action[2] += z_perturbation
+                # else:
+                #     # perturb x, y, and z by small amount
+                #     xy_perturbation = np.random.normal(0, 0.01, size=(2,))
+                #     perturbed_action[0:2] += xy_perturbation
+                #     z_perturbation = -1
+                #     while z_perturbation <= 0:
+                #         z_perturbation = np.random.normal(0, 0.01)
+                #     perturbed_action[2] += z_perturbation
+
+
+                # Sampling corrective behavior
+                # randomly sample a correct waypoint from a segment of waypoints leading up to the expert keypoint
+                if idx == 0:
+                    half = keypoints[idx] // 2
+                    waypoint_segment = waypoints[0 + half : keypoints[idx]]
+                else:
+                    half = (keypoints[idx] - keypoints[idx-1]) // 2
+                    waypoint_segment = waypoints[keypoints[idx-1] + half : keypoints[idx]]
+                random_idx = np.random.choice(waypoint_segment)
+                corrective_idx.append(random_idx)
+                corrective_action = dense_actions[random_idx]
+
+                # insert perturbed action and correction action to actions
+                actions = np.insert(actions, p_count * 2 + idx, np.vstack([perturbed_action, corrective_action]), axis=0)
+                p_count += 1
+            print(np.round(actions,3))
 
         # 3. stepping
         step = 0
@@ -138,16 +205,16 @@ class RolloutGenerator(object):
                 if step >= len(actions):
                     return
                 act_result = ActResult(actions[step]) # selects the ith keypoint as action
-            if perturb_count <= len(perturbed_idx) - 1:
-                if step == 2 * perturb_count + perturbed_idx[perturb_count]:
-                    print(f"Step {step} | pred action: {np.round(act_result.action, 3)} --> (perturbed waypoint {keypoints[step - 2 * perturb_count]})")
-                elif step == 2 * perturb_count + perturbed_idx[perturb_count] + 1:
-                    print(f"Step {step} | pred action: {np.round(act_result.action, 3)} --> (corrected waypoint {keypoints[step - 2 * perturb_count - 1]})")
+                if perturb_count <= len(perturbed_idx) - 1:
+                    if step == 2 * perturb_count + perturbed_idx[perturb_count]:
+                        print(f"Step {step} | pred action: {np.round(act_result.action, 3)} --> (perturbed waypoint {keypoints[step - 2 * perturb_count]})")
+                    elif step == 2 * perturb_count + perturbed_idx[perturb_count] + 1:
+                        print(f"Step {step} | pred action: {np.round(act_result.action, 3)} --> (corrected waypoint {keypoints[step - 2 * perturb_count - 1]} = dense waypoint idx {corrective_idx[perturb_count]})")
+                    else:
+                        print(f"Step {step} | pred action: {np.round(act_result.action, 3)} --> (waypoint {keypoints[step - 2 * perturb_count]})")
                 else:
                     print(f"Step {step} | pred action: {np.round(act_result.action, 3)} --> (waypoint {keypoints[step - 2 * perturb_count]})")
-            else:
-                print(f"Step {step} | pred action: {np.round(act_result.action, 3)} --> (waypoint {keypoints[step - 2 * perturb_count]})")
-                
+                    
 
             # 3-2. take interactive actions
             if interactive:
@@ -246,7 +313,6 @@ class RolloutGenerator(object):
             if transition.terminal or timeout:
                 # If the agent gives us observations then we need to call act
                 # one last time (i.e. acting in the terminal state).
-                print("terminal~~")
                 if len(act_result.observation_elements) > 0:
                     prepped_data = {k: torch.tensor([v], device=self._env_device) for k, v in obs_history.items()}
                     if agent is not None:
@@ -266,6 +332,29 @@ class RolloutGenerator(object):
 
             # TODO_JJL how to deal with termination or too early termination
             if transition.info.get("needs_reset", transition.terminal):
+                step += 1
+                for cam in CAMERAS:
+                    rgb = obs[f'{cam}_rgb'] # (3, IMAGE_SIZE, IMAGE_SIZE)
+                    rgb = Image.fromarray(rgb.T).rotate(-90)
+                    if not replay_ground_truth:
+                        rgb.save(os.path.join(multiview_img_folder, f"{cam}", f"{step}.png"))
+                    else:
+                        if step == 0:
+                            img_name = 0
+                        else:
+                            if perturb_count <= len(perturbed_idx) - 1:
+                                if step == 2 * perturb_count + perturbed_idx[perturb_count] + 1:
+                                    img_name = f"{keypoints[step - 2 * perturb_count - 1]}_1_perturbed"
+                                elif step == 2 * perturb_count + perturbed_idx[perturb_count] + 2:
+                                    img_name = f"{keypoints[step - 2 * perturb_count - 2]}_2_corrected"
+                                    perturb_count += 1
+                                else:
+                                    img_name = f"{keypoints[step - 2 * perturb_count - 1]}"
+                            else:
+                                img_name = f"{keypoints[step - 2 * perturb_count - 1]}"
+                        rgb.save(os.path.join(multiview_img_folder, f"{cam}", f"{img_name}.png"))
+                    # if interactive:
+                    rgb.save(os.path.join(multiview_img_folder, f"{cam}", "current.png"))
                 return
 
             if not replay_ground_truth and step == episode_length:
